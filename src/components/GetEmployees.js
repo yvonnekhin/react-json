a005f2e22677402753b223b47db10042e4a3d9fa
yvonnekhin <yvonnekhin@hotmail.com>
2019-09-09 18:04:54 +0800

    changes

--- src/components/GetEmployees.js
+++ src/components/GetEmployees.js
@@ -6,42 +6,6 @@ import "react-table/react-table.css";
 
 import matchSorter from "match-sorter";
 
-const data = [
-  {
-    userId: "rirani",
-    jobTitleName: "Developer",
-    firstName: "Romin",
-    lastName: "Irani",
-    preferredFullName: "Romin Irani",
-    employeeCode: "E1",
-    region: "CA",
-    phoneNumber: "408-1234567",
-    emailaddress: "romin.k.irani@gmail.com"
-  },
-  {
-    userId: "nirani",
-    jobTitleName: "Developer",
-    firstName: "Neil",
-    lastName: "Irani",
-    preferredFullName: "Neil Irani",
-    employeeCode: "E2",
-    region: "CA",
-    phoneNumber: "408-1111111",
-    emailaddress: "neilrirani@gmail.com"
-  },
-  {
-    userId: "thanks",
-    jobTitleName: "Program Directory",
-    firstName: "Tom",
-    lastName: "Hanks",
-    preferredFullName: "Tom Hanks",
-    employeeCode: "E3",
-    region: "CA",
-    phoneNumber: "408-2222222",
-    emailaddress: "tomhanks@gmail.com"
-  }
-];
-
 class GetEmployees extends Component {
   constructor(props) {
     super(props);
@@ -108,7 +72,7 @@ class GetEmployees extends Component {
     return (
       <div>
         <ReactTable
-          data={data}
+          data={employees}
           filterable
           defaultFilterMethod={(filter, row) =>
             String(row[filter.id]) === filter.value
